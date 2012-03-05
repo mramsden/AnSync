0812355e4a609e17826db0e819b84bb61053bb2d
Marcus Ramsden <marcus@beyondthecorner.co.uk>
2012-03-05 22:07:05 +0000

    Initial Commit

---
+++ AnSync/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  AnSync
+//
+//  Created by Marcus Ramsden on 05/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ AnSync/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  AnSync
+//
+//  Created by Marcus Ramsden on 05/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
